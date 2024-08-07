581583a6e5686556e63ae138efc93f6ddb0ea895
Maarten van Keulen <maartenvkeulen@proton.me>
2024-08-07 18:30:44 +0200

    recursion version, memory limit exceeded

--- src/5-longest-palindromic-substring.cpp
+++ src/5-longest-palindromic-substring.cpp
@@ -1,40 +1,47 @@
 #include <iostream>
-
-#include <string_view>
+#include <string>
+#include <unordered_map>
 
 class Solution {
 public:
     std::string longestPalindrome(std::string s) {
-        const std::string_view sv(s);
+        std::string longest{s};
 
-        for (size_t size = sv.size(); size > 0; size--) {
-            for (size_t i = 0; i <= (sv.size() - size); i++) {
-                auto slice = sv.substr(i, size);
+        for (size_t size = 0; size <= s.size(); size++) {
+            for (size_t i = 0; i <= (s.size() - size); i++) {
+                auto slice = s.substr(i, size);
 
                 if (is_palindrome(slice)) {
-                    return std::string{slice};
+                    longest = std::string{slice};
                 }
             }
         }
 
-        return "";
+        return longest;
     }
 
-    bool is_palindrome(std::string_view s) {
-        for (size_t i = 0, j = s.size() - 1; i < (s.size() / 2); i++, j--) {
-            if (s.at(i) != s.at(j)) {
-                return false;
-            }
+    bool is_palindrome(std::string const& s) {
+        static std::unordered_map<std::string, bool> cache;
+
+        if (s.size() < 2) {
+            return true;
         }
 
-        return true;
+        if (cache.count(s)) {
+            return cache[s];
+        }
+
+        if (s[0] != s[s.size() - 1]) {
+            return cache[s] = false;
+        }
+
+        return cache[s] = is_palindrome(s.substr(1, s.size() - 2));
     }
 };
 
 int main() {
     Solution s;
-    auto result = s.longestPalindrome("dtgrtoxuybwyfskikukcqlvprfipgaygawcqnfhpxoifwgpnzjfdnhpgmsoqzlpsaxmeswlhzdxoxobxysgmpkhcylvqlzenzhzhnakctrliyyngrquiuvhpcrnccapuuwrrdufwwungaevzkvwbkcietiqsxpvaaowrteqgkvovcoqumgrlsxvouaqzwaylehybqchsgpzbkfugujrostopwhtgrnrggocprnxwsecmvofawkkpjvcchtxixjtrnngrzqpiwywmnbdnjwqpmnifdiwzpmabosrmzhgdwgcgidmubywsnshcgcrawjvfiuxzyzxsbpfhzpfkjqcpfyynlpshzqectcnltfimkukopjzzmlfkwlgbzftsddnxrjootpdhjehaafudkkffmcnimnfzzjjlggzvqozcumjyazchjkspdlmifvsciqzgcbehqvrwjkusapzzxyiwxlcwpzvdsyqcfnguoidiiekwcjdvbxjvgwgcjcmjwbizhhcgirebhsplioytrgjqwrpwdciaeizxssedsylptffwhnedriqozvfcnsmxmdxdtflwjvrvmyausnzlrgcchmyrgvazjqmvttabnhffoe");
-
+    auto result = s.longestPalindrome("ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy");
     std::cout << "Result: " << result << std::endl;
     return 0;
 }
