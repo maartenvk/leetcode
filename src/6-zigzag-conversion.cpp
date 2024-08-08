af8e22d8b055bc6d0775eb59b5e4e72ab16a5288
Maarten van Keulen <maartenvkeulen@proton.me>
2024-08-08 20:34:13 +0200

    accepted solution using streams approach

--- src/6-zigzag-conversion.cpp
+++ src/6-zigzag-conversion.cpp
@@ -1,76 +1,47 @@
 #include <cstddef>
 #include <iostream>
 #include <sstream>
-#include <stdexcept>
-#include <vector>
-
-template <typename T>
-class matrix {
-public:
-    std::vector<T> data;
-    size_t w, h;
-
-    matrix(size_t w, size_t h) : data(w*h), w{w}, h{h} {}
-
-    typename std::vector<T>::iterator operator[](size_t row) {
-        if (row > h) {
-            std::cerr << "Indexing at " << row << std::endl;
-            throw std::out_of_range{"accessing"};
-        }
-
-        return data.begin() + (row * w);
-    }
-};
 
 class Solution {
 public:
     std::string convert(std::string s, int numRows) {
-        matrix<char> m(s.length(), size_t(numRows));
-        size_t y{0}, x{0};
-        bool going_up{false};
+        std::ostringstream streams[numRows];
+
+        if (numRows == 1) {
+            return s;
+        }
+
+        // w: s length, h: numRows
+        ssize_t y {0};
+        bool falling {true};
         for (size_t i{0}; i < s.size(); i++) {
-            m[y][x] = s.at(i);
+            streams[y] << s.at(i);
 
-            if (going_up) {
-                x++;
-                if (--y == 0) {
-                    going_up = false;
-                }
+            if (falling) {
+                y++;
             } else {
-                if (++y == m.h) {
-                    going_up = true;
-                    y -= 2, x++;
-                }
+                y--;
+            }
+
+            if (y == ssize_t(numRows)) {
+                y -= 2;
+                falling = false;
+            }
+
+            if (y < 0) {
+                y = 0;
+            }
+
+            if (y == 0) {
+                falling = true;
             }
         }
 
-        std::ostringstream os{""};
-
-        // flatten vector
-        for (char c : m.data) {
-            if (c == '\0') {
-                continue;
-            }
-
-            os << c;
+        for (size_t i = 1; i < size_t(numRows); i++) {
+            streams[0] << streams[i].str();
         }
 
-        return os.str();
-    }
-
-    void print_matrix(matrix<char> m) {
-        for (size_t y = 0; y < m.h; y++) {
-            for (size_t x = 0; x < m.w; x++) {
-                char c = m[y][x];
-                if (c == '\0') {
-                    c = '-';
-                }
-
-                std::cout << c;
-            }
-
-            std::cout << std::endl;
-        }
+        return streams[0].str();
     }
 };
 
