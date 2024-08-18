7108f8c282428f9351460fa0e78510e3504376e7
Maarten van Keulen <maartenvkeulen@proton.me>
2024-08-18 18:33:58 +0200

    base for 10

---
+++ src/10-regex-matching.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Solution {
+public:
+    bool isMatch(std::string s, std::string p) {
+    }
+};
+
+int main() {
+    Solution s;
+    auto result = s.isMatch("aa", "a");
+
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
