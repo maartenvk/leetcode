92d3f44de5937c8e5cb0debeaab1a5d60a635769
Maarten van Keulen <maartenvkeulen@proton.me>
2024-08-18 17:00:34 +0200

    faster solution

--- src/8-atoi.cpp
+++ src/8-atoi.cpp
@@ -4,54 +4,44 @@
 class Solution {
 public:
     int myAtoi(std::string s) {
-        int32_t output {0};
-
-        constexpr int32_t upper_limit{std::numeric_limits<int32_t>::max() / 10};
+        int64_t output {0};
 
         bool has_read {false};
         bool is_signed {false};
         for (std::string::const_iterator it = s.begin(); it != s.end(); it++) {
-            if (!has_read && *it == ' ') { // skip whitespace
-                continue;
-            }
+            if (!has_read) {
+                if (*it == ' ') {
+                    continue;
+                }
 
-            if (!has_read && *it == '-') { // if not started & is minus sign
-                is_signed = true;
-                has_read = true;
-                continue;
-            }
+                if (*it == '-') {
+                    is_signed = true;
+                    has_read = true;
+                    continue;
+                }
 
-            if (!has_read && *it == '+') { // skip over plus sign
-                has_read = true;
-                continue;
+                if (*it == '+') {
+                    has_read = true;
+                    continue;
+                }
             }
             
-            if (*it < '0' || *it > '9') { // if not a digit
+            if (*it < '0' || *it > '9') {
                 break;
             }
 
-            int digit = (*it) - '0';
-            if (output > upper_limit) [[unlikely]] {
-                if (is_signed) {
-                    return std::numeric_limits<int32_t>::min();
-                } else {
-                    return std::numeric_limits<int32_t>::max();
-                }
-            }
-
-            output *= 10;
-
-            auto add_limit {std::numeric_limits<int32_t>::max() - digit};
-            if (output > add_limit) [[unlikely]] {
-                if (is_signed) {
-                    return std::numeric_limits<int32_t>::min();
-                } else {
-                    return std::numeric_limits<int32_t>::max();
-                }
-            }
-
-            output += digit;
             has_read = true;
+
+            int digit = (*it) - '0';
+            output *= 10;
+            output += digit;
+            if (output > std::numeric_limits<int32_t>::max()) {
+                if (is_signed) {
+                    return std::numeric_limits<int32_t>::min();
+                } else {
+                    return std::numeric_limits<int32_t>::max();
+                }
+            }
         }
 
         if (is_signed) {
@@ -64,7 +54,7 @@ public:
 
 int main() {
     Solution s;
-    auto result = s.myAtoi("words and 987");
+    auto result = s.myAtoi("0-1");
 
     std::cout << "Result: " << result << std::endl;
     return 0;
