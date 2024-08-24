785ddab0336098e6e77afc7a5000d48da4aa89a1
Maarten van Keulen <maartenvkeulen@proton.me>
2024-08-24 13:46:28 +0200

    faster code

--- src/10-regex-matching.cpp
+++ src/10-regex-matching.cpp
@@ -2,24 +2,23 @@
 #include <string_view>
 
 class Solution {
-public:
-    bool isMatch(std::string_view s, std::string_view p, size_t s_idx = 0, size_t p_idx = 0) {
-        char p_prev {p_idx == 0 ? '\0' : p.at(p_idx - 1)};
-        bool has_next = (p_idx + 1) < p.size();
+    size_t s_size, p_size;
 
-        if (p_idx >= p.size()) { // exhausted pattern
-            return s_idx >= s.size();
+    bool is_match(char const* const s, char const* const p, size_t s_idx = 0, size_t p_idx = 0) {
+        if (p_idx >= p_size) [[unlikely]] { // exhausted pattern
+            return s_idx >= s_size;
         }
 
-        if (s_idx >= s.size()) { // exhausted string before pattern
+        if (s_idx >= s_size) [[unlikely]] { // exhausted string before pattern
             // if continuously followed with letters and '*', it can still match
-            if (p.at(p_idx) == '*') {
+            if (p[p_idx] == '*') {
                 p_idx++;
             }
 
-            for (bool valid = p_idx < p.size(); valid; valid = p_idx < p.size()) {
-                has_next = (p_idx + 1) < p.size();
-                char next = has_next ? p.at(p_idx + 1) : '\0';
+            bool has_next;
+            for (bool valid = p_idx < p_size; valid; valid = p_idx < p_size) {
+                has_next = (p_idx + 1) < p_size;
+                char next = has_next ? p[p_idx + 1] : '\0';
 
                 if (has_next && next == '*') {
                     p_idx += 2;
@@ -28,47 +27,57 @@ public:
                 }
             }
 
-            return p.at(p_idx - 1) == '*';
+            return p[p_idx - 1] == '*';
         }
 
-        char s_cur {s.at(s_idx)};
-        char p_cur {p.at(p_idx)};
+        char s_cur {s[s_idx]};
+        char p_cur {p[p_idx]};
 
-        if (s_cur == p_cur || p_cur == '.') {
-            if (isMatch(s, p, s_idx + 1, p_idx + 1)) {
+        if (s_cur == p_cur || p_cur == '.') [[likely]] {
+            if (is_match(s, p, s_idx + 1, p_idx + 1)) {
                 return true;
             }
         }
 
         if (p_cur == '*') {
             // is current string char valid?
+            char p_prev {p[p_idx - 1]};
             if (p_prev == '.' || p_prev == s_cur) {
                 // it matches. continue
-                bool result = isMatch(s, p, s_idx + 1, p_idx);
+                bool result = is_match(s, p, s_idx + 1, p_idx);
                 if (result) {
                     return true;
                 }
 
                 // if it does not match, try skipping the current pattern char.
-                result = isMatch(s, p, s_idx + 1, p_idx + 1);
+                result = is_match(s, p, s_idx + 1, p_idx + 1);
                 if (result) {
                     return true;
                 }
 
                 // there was only one present and the next character was the repeating char.
-                return isMatch(s, p, s_idx, p_idx + 1);
+                return is_match(s, p, s_idx, p_idx + 1);
             } else {
                 // there was only one present, skip
-                return isMatch(s, p, s_idx, p_idx + 1);
+                return is_match(s, p, s_idx, p_idx + 1);
             }
         }
 
         // with '*' if there are zero present, skip
-        if (has_next && p.at(p_idx + 1) == '*') {
-            return isMatch(s, p, s_idx, p_idx + 2);
+        bool has_next = (p_idx + 1) < p_size;
+        if (has_next && p[p_idx + 1] == '*') {
+            return is_match(s, p, s_idx, p_idx + 2);
         }
 
         return false;
+    }
+
+public:
+    bool isMatch(std::string_view s, std::string_view p) {
+        s_size = s.size();
+        p_size = p.size();
+
+        return is_match(s.data(), p.data());
     }
 };
 
