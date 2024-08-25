d953064cbab39f4cbd90ae2629e6b663f6751f15
Maarten van Keulen <maartenvkeulen@proton.me>
2024-08-25 11:44:16 +0200

    accepted solution

---
+++ src/11-container-with-most-water.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <cmath>
+
+class Solution {
+public:
+    int maxArea(std::vector<int>& height) {
+        int const* const height_nonvec = height.data();
+        int greatest_volume {0};
+
+        int i {0}, j {int(height.size()) - 1};
+        int a {height_nonvec[i]}, b {height_nonvec[j]};
+        while (j > i) {
+            int height = std::min(a, b);
+            int width {j - i};
+
+            int calculated_volume = height * width;
+            if (calculated_volume > greatest_volume) {
+                greatest_volume = calculated_volume;
+            }
+
+            if (a < b) {
+                a = height_nonvec[++i];
+            } else {
+                b = height_nonvec[--j];
+            }
+        }
+
+        return greatest_volume;
+    }
+};
+
+int main() {
+    Solution s;
+
+    std::vector<int> vec {1,8,6,2,5,4,8,3,7};
+    auto result = s.maxArea(vec);
+
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
